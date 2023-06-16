f35c25ff42b234bde22ad9f0980632f13379372a
mebi <mebiali04@gmail.com>
2023-06-16 18:53:56 +0500

    extract ok

--- src/etl_service/extract.py
+++ src/etl_service/extract.py
@@ -1,24 +1,29 @@
-import requests
 import os
+
+import requests  # type: ignore
 from base_logger import logger
 from dotenv import load_dotenv
+
 load_dotenv()
 
-def get_api_data(url: str) -> dict:
+
+def get_api_data(url: str):
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
         err_msg = f"Error {response.status_code} occurred while accessing {url}"
         logger.error(err_msg)
-        raise response.raise_for_status()
+        response.raise_for_status()
+
 
 urls = {
     "appointment": f"{os.getenv('BASE_URL')}/appointment",
     "councillor": f"{os.getenv('BASE_URL')}/councillor",
     "patient_councillor": f"{os.getenv('BASE_URL')}/patient_councillor",
-    "rating": f"{os.getenv('BASE_URL')}/rating"
+    "rating": f"{os.getenv('BASE_URL')}/rating",
 }
+
 
 if __name__ == "__main__":
     urls = {key: get_api_data(val) for key, val in urls.items()}
