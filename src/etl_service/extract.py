328170f2584b0c638f17cc97f633c11f3a3d45a7
mebi-ali <mehboob.ali@xloopdigital>
2023-06-19 16:57:54 +0500

    updated

--- src/etl_service/extract.py
+++ src/etl_service/extract.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def get_api_data(url: str):
     response = requests.get(url)
     if response.status_code == 200:
@@ -18,12 +17,35 @@ def get_api_data(url: str):
 
 
 urls = {
-    "appointment": f"{os.getenv('BASE_URL')}/appointment",
-    "councillor": f"{os.getenv('BASE_URL')}/councillor",
-    "patient_councillor": f"{os.getenv('BASE_URL')}/patient_councillor",
-    "rating": f"{os.getenv('BASE_URL')}/rating",
+    "appointment": "https://xloop-dummy.herokuapp.com/appointment",
+    "councillor": "https://xloop-dummy.herokuapp.com/councillor",
+    "patient_councillor": "https://xloop-dummy.herokuapp.com/patient_councillor",
+    "rating": "https://xloop-dummy.herokuapp.com/rating"
 }
 
 
 if __name__ == "__main__":
     urls = {key: get_api_data(val) for key, val in urls.items()}
+
+
+
+# def get_api_data(url: str):
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         return response.json()
+#     else:
+#         err_msg = f"Error {response.status_code} occurred while accessing {url}"
+#         logger.error(err_msg)
+#         response.raise_for_status()
+
+
+# urls = {
+#     "appointment": f"{os.getenv('BASE_URL')}/appointment",
+#     "councillor": f"{os.getenv('BASE_URL')}/councillor",
+#     "patient_councillor": f"{os.getenv('BASE_URL')}/patient_councillor",
+#     "rating": f"{os.getenv('BASE_URL')}/rating",
+# }
+
+
+# if __name__ == "__main__":
+#     urls = {key: get_api_data(val) for key, val in urls.items()}
--- src/etl_service/redis_connector.py
+++ src/etl_service/redis_connector.py
@@ -1,6 +1,6 @@
 import redis 
 
-redis_host = '0.0.0.0' 
+redis_host = 'redis' 
 redis_port = 6379
 redis_db = 0
 redis_client = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
